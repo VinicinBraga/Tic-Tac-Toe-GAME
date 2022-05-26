c22b6382b1517d4d60b8f0ac7008952e85fd1776
VinicinBraga <vinicius@bragamatta.com.br>
2022-05-26 17:41:21 -0300

    Combinations to win

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("[data-board]");
 
 let isCircleTurn;
+const winningCombinations = [
+  [0, 1, 2][(3, 4, 5)][(6, 7, 8)][(0, 3, 6)][(1, 4, 7)][(2, 5, 8)][(0, 4, 8)][
+    (6, 4, 2)
+  ],
+];
 
 const startGame = () => {
   for (const cell of cellElements) {
@@ -32,10 +37,11 @@ const handleClick = (e) => {
   //Colocar(X ou Circle)
   const cell = e.target;
   const classToadd = isCircleTurn ? "circle" : "x";
-
   placeMark(cell, classToadd);
+
   //Verificar vitoria
   //Verificar Empate
+
   //Mudar simbolo
   swapTurns();
 };
