5b6c1a02e5d27e2fb82aeea9482a9c5516047362
VinicinBraga <vinicius@bragamatta.com.br>
2022-05-26 17:25:59 -0300

    Change board and cells class to X or Circle

---
+++ script.js
@@ -0,0 +1,43 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+
+let isCircleTurn;
+
+const startGame = () => {
+  for (const cell of cellElements) {
+    cell.addEventListener("click", handleClick, { once: true });
+  }
+  isCircleTurn = false;
+  board.classList.add("x");
+};
+
+const placeMark = (cell, classToadd) => {
+  cell.classList.add(classToadd);
+};
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+
+  board.classList.remove("circle");
+  board.classList.remove("x");
+
+  if (isCircleTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+};
+
+const handleClick = (e) => {
+  //Colocar(X ou Circle)
+  const cell = e.target;
+  const classToadd = isCircleTurn ? "circle" : "x";
+
+  placeMark(cell, classToadd);
+  //Verificar vitoria
+  //Verificar Empate
+  //Mudar simbolo
+  swapTurns();
+};
+
+startGame();
