c0a789785ba09825d4aaefb1d44739c69ca30d2c
VinicinBraga <vinicius@bragamatta.com.br>
2022-05-27 14:47:50 -0300

    Check for Win Function - Done

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("[data-board]");
-
+const winningMessage = document.querySelector("[data-winning-message]");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
 let isCircleTurn;
+
 const winningCombinations = [
-  [0, 1, 2][(3, 4, 5)][(6, 7, 8)][(0, 3, 6)][(1, 4, 7)][(2, 5, 8)][(0, 4, 8)][
-    (6, 4, 2)
-  ],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
 ];
 
 const startGame = () => {
@@ -14,6 +23,25 @@ const startGame = () => {
   }
   isCircleTurn = false;
   board.classList.add("x");
+};
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Empate!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn
+      ? "Circle Won!"
+      : "X Won!";
+  }
+  winningMessage.classList.add("show-winning-message");
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
 };
 
 const placeMark = (cell, classToadd) => {
@@ -37,11 +65,14 @@ const handleClick = (e) => {
   //Colocar(X ou Circle)
   const cell = e.target;
   const classToadd = isCircleTurn ? "circle" : "x";
+
   placeMark(cell, classToadd);
-
   //Verificar vitoria
+  const isWin = checkForWin(classToadd);
+  if (isWin) {
+    endGame(false);
+  }
   //Verificar Empate
-
   //Mudar simbolo
   swapTurns();
 };
